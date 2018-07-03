56dccfb5dca8743216a222c402aed7e1420f7a55
fabiojesus <fabio_jesus@live.com>
2018-07-03 21:31:19 +0100

    Itty Bitty Committy

--- src/Config/connect.js
+++ src/Config/connect.js
@@ -1,6 +1,7 @@
 function  connection(){
   const mongoose = require("mongoose");
-  const URL = "mongodb://localhost/vigil-alpha";
+  //const URL = "mongodb://localhost/vigil-alpha";
+  const URL = "mongodb://fabiojesus:tuna123@ds161700.mlab.com:61700/heroku_8xzfhp1l";
 
   //Connect to mongodb
   mongoose.connect(URL);
