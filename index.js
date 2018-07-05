6ef22dc6fef7449b99f980f58cd064d40fa127be
fabiojesus <fabio_jesus@live.com>
2018-07-05 20:21:52 +0100

    itty bitty titty committy

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 let schema = require('./src/Graph/Schemas/Schema');
 let resolver = require('./src/Graph/Resolver/Resolver');
 
