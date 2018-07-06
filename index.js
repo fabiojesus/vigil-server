6c5e1c1bfb50bde6d05e43cc1585a0acd7cdde8f
fabiojesus <fabio_jesus@live.com>
2018-07-06 19:44:36 +0100

    itty bitty gitty committy

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const gqHTTP = require("express-graphql");
 const connect = require('./src/Config/connect')();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 //app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,15 +16,10 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-
 let schema = require('./src/Graph/Schemas/Schema');
 let resolver = require('./src/Graph/Resolver/Resolver');
+
+app.use(cors());
 
 
 app.use("/", gqHTTP({schema:schema,    rootValue: resolver,    graphiql: true}));
